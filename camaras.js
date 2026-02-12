564a7a7fa819106ccf57e09e9a916cd91cd74b7d
lazerboy404 <69212410+lazerboy404@users.noreply.github.com>
2026-02-12 08:32:46 -0600

    Add files via upload

---
+++ camaras.js
@@ -0,0 +1,7 @@
+// Agregamos 'export' para que otros archivos puedan usar esta lista
+export const datosMasivos = [
+    { id: "AXIS P3267-LV-01", ubicacion: "Recepcion", coordenadas: "20.12881, -98.80509", modelo: "AXIS P3267-LV", herraje: "AXIS TQ9301", notas: "ESQUINA INTERIOR" },
+    { id: "AXIS P3267-LV-02", ubicacion: "Recepcion_entrada", coordenadas: "20.12881, -98.80509", modelo: "AXIS P3267-LV", herraje: "AXIS TQ9301", notas: "ESQUINA INTERIOR" },
+    // ... Pega aquí el resto de tus 90 filas ...
+    { id: "AXIS P3267-LV-90", ubicacion: "Ejemplo Final", coordenadas: "...", modelo: "...", herraje: "...", notas: "..." }
+];
